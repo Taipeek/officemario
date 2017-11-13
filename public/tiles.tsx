1181a384f57b896326f9194aaa6faf093515d876
Arun P <arunparamanathan@ksu.edu>
2017-11-13 11:15:01 -0600

    i have no idea

--- public/tiles.tsx
+++ public/tiles.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="tiles" tilewidth="32" tileheight="32" tilecount="4" columns="2">
  <image source="tiles.png" width="64" height="64"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <properties>
+    <property name="solid" type="bool" value="true"/>
+   </properties>
+  </objectgroup>
+ </tile>
 </tileset>
