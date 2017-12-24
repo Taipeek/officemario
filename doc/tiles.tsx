5e9b28793013e41f47e167e5d2b18aa86481997f
Martin Horejš <taipeek@gmail.com>
2017-12-24 12:41:05 +0100

    github pages

---
+++ doc/tiles.tsx
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="tiles.png" width="64" height="64"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <properties>
+    <property name="solid" type="bool" value="true"/>
+   </properties>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <properties>
+    <property name="solid" type="bool" value="true"/>
+   </properties>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <properties>
+    <property name="solid" type="bool" value="true"/>
+   </properties>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <properties>
+    <property name="solid" type="bool" value="false"/>
+   </properties>
+  </objectgroup>
+ </tile>
+</tileset>
